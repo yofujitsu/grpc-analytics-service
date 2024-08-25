514fdab82f77641bdf1f6b58ee78ef910467eaef
yofujitsu <yofujitsuuu@gmail.com>
2024-08-25 13:11:58 +0300

    first commit

---
+++ src/main/java/com/yofujitsu/grpcanalyticsservice/GrpcAnalyticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yofujitsu.grpcanalyticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcAnalyticsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcAnalyticsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yofujitsu/grpcanalyticsservice/GrpcAnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yofujitsu.grpcanalyticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcAnalyticsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
